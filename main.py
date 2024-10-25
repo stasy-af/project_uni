5705ad998b0d01bbbfb4581882538a3b8ccabd44
Мария Хисматуллова <anansy.pineapple@gmail.com>
2024-10-25 23:23:17 +0300

    updated previous version (on main)

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print ('hello wooooooorld')
+import asyncio
+from aiogram import Bot, Dispatcher
+from handlers import router
+
+
+BOT_TOKEN='7581394986:AAG23-Xq_TVi1n-4KpFyCyyoh87vR3YqSmA'
+bot = Bot(token=BOT_TOKEN)
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router=router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
